07b35f5038608dfbe9cf89f9d9f278cd1d7444fd
Dennis Zeballos <deniszeballos15@gmail.com>
2025-05-13 20:55:13 -0400

    funcionalidad de los botones añadida pero falta hacer validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@
 import './App.css'
 
 import Grilla from './components/Grilla';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [pos, setPos] = useState({ x: 0, y: -1 })
+
   return (
     <div className="app-contenedor">
-        <Grilla />
-        <div className="botones-contenedor">
-          <button>Arriba</button>
-          <button>Abajo</button>
-          <button>Derecha</button>
-          <button>Izquierda</button>
-          <button>Girar Der</button>
-          <button>Girar Izq</button>
-        </div>
+      <Grilla pos={pos} />
+
+      <div className="botones-contenedor">
+        <button onClick={() => setPos({ x: pos.x, y: pos.y - 1 })}>Arriba</button>
+        <button onClick={() => setPos({ x: pos.x, y: pos.y + 1 })}>Abajo</button>
+        <button onClick={() => setPos({ x: pos.x + 1, y: pos.y })}>Derecha</button>
+        <button onClick={() => setPos({ x: pos.x - 1, y: pos.y })}>Izquierda</button>
+        <button onClick={() => setPos({ x: pos.x, y: pos.y })}>Girar Der</button>
+        <button onClick={() => setPos({ x: pos.x, y: pos.y })}>Girar Izq</button>
+      </div>
     </div>
   )
 }
--- src/components/Grilla.jsx
+++ src/components/Grilla.jsx
@@ -5,37 +5,46 @@ import Celda from './Celda';
 import flechaImg from '../assets/flecha.png';
 
 
-function Grilla() {
+
+function Grilla({ pos }) {
 
     const filas = 5
     const columnas = 5
     const alto = 550
     const ancho = 550
+
     let grilla = []
 
-    for(let i = 0; i < filas; i++){
-        for(let j = 0; j < columnas; j++){
-            grilla = [...grilla, {x: i, y: j}]
+    for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < columnas; j++) {
+            grilla = [...grilla, { x: i, y: j }]
         }
     }
 
-    console.log(grilla);
+    // console.log(grilla);
+
+    const tranformStyle = `translateX(${pos.x * 100}%) translateY(${pos.y * 100}%)`
 
     return (
-        <div className="grilla" style={{'--alto': `${alto}px`, '--ancho': `${ancho}px`}}>
+        <div className="grilla" style={{
+            '--alto': `${alto}px`,
+            '--ancho': `${ancho}px`
+        }}>
             {
                 grilla.map(actual => {
-                return <Celda key={`{${actual.x}, ${actual.y}}`} alto={alto / filas} ancho={ancho / columnas} />
-            })
+                    return <Celda key={`{${actual.x}, ${actual.y}}`} alto={alto / filas} ancho={ancho / columnas} />
+                })
             }
-            <div className="flecha-contenedor">
-                <img src={flechaImg} alt="flecha" />
+            {/* <Bot estilosExtra={{tranform: `${tranformStyle}`}}  alto={alto / filas} ancho={ancho / columnas} /> */}
+            <div className="bot-contenedor" style={{
+                '--alto': `${alto}px`, 
+                '--ancho': `${ancho}px`,
+                transform: `${tranformStyle}`
+            }}>
+                <img src={flechaImg} alt="bot" />
             </div>
         </div>
     )
-
-
-    
 
 }
 
